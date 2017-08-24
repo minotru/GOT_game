ae81744aa5f7c487c5273e0e5611d9104456a687
Simon <senich10@mail.ru>
2017-08-24 16:21:40 +0300

    map.json updated and static from Map is removed.

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,11 @@
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonSerializer;
+import com.google.gson.internal.bind.JsonTreeReader;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -11,63 +15,37 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Test {
-	public static void main(String args[]) {
-		Map.sectors = new ArrayList<Sector>();
-		int i=0;
-		Map.sectors.add(new Sector((new SectorDataBuilder()).
-				setName("Castle Black").setCastle(0).setCrown(1).setSupply(0).setLandscape(SectorData.Landscape.ground).create(),
-				i++, new ArrayList<Sector>()));
-		Map.sectors.add(new Sector((new SectorDataBuilder()).
-				setName("Bay of Ice").setCastle(0).setCrown(0).setSupply(0).setLandscape(SectorData.Landscape.water).create(),
-				i++, new ArrayList<Sector>()));
-		Map.sectors.add(new Sector((new SectorDataBuilder()).
-				setName("Karhold").setCastle(1).setCrown(0).setSupply(0).setLandscape(SectorData.Landscape.ground).create(),
-				i++, new ArrayList<Sector>()));
-		Map.sectors.add(new Sector((new SectorDataBuilder()).
-				setName("The Shivering Sea").setCastle(0).setCrown(0).setSupply(0).setLandscape(SectorData.Landscape.water).create(),
-				i++, new ArrayList<Sector>()));
-		Map.sectors.add(new Sector((new SectorDataBuilder()).
-				setName("Winterfell").setCastle(2).setCrown(1).setSupply(1).setLandscape(SectorData.Landscape.ground).create(),
-				i++, new ArrayList<Sector>()));
-		
-//		GsonBuilder builder = new GsonBuilder();
-//		Gson gson = builder.create();
-//		//System.out.println(gson.toJson(sd));
-////		System.out.println(Landscape.ground);
-//		try (FileWriter file = new FileWriter("sector.json"))
-//		{
-//			file.write(gson.toJson(sd));
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		SectorData sk = new SectorData();
-//		try(JsonReader reader = new JsonReader(new FileReader("sector.json"))){
-//			sk = gson.fromJson(reader, SectorData.class);
-//		}catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		System.out.println(sk.name+" "+sk.castle+" "+sk.crown+" "+sk.supply+" "+sk.landscape);
-		GsonBuilder builder = new GsonBuilder();
-		Gson gson = builder.create();
-		//System.out.println(gson.toJson(sd));
-//		System.out.println(Landscape.ground);
-		try (FileWriter file = new FileWriter("sector.json"))
-		{
-			file.write(gson.toJson(Map.sectors));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    public static void main(String args[]) {
+        GsonBuilder gsonBuilder = new GsonBuilder().setPrettyPrinting();
+        Gson gson = gsonBuilder.create();
+        Map map = new Map();
+        //массив читает правильно
+        try (JsonReader reader = new JsonReader(new FileReader("map_in.json"))) {
+            map.sectors = gson.fromJson(reader, map.sectors.getClass());
+            System.out.println(gson.toJson(map));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //целый объект тоже отлично выводит
+        try (JsonWriter writer = new JsonWriter(new FileWriter("map.json"))) {
+            gson.toJson(map, map.getClass(), writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //@fixme
+        //должно быть логичным, что он может прочитать то, что сам вывел
+        //но нет, кидает ошибку, что ожидался объект, а ты мне дал число
+//        try (JsonReader reader = new JsonReader(new FileReader("map.json"))) {
+//            map = gson.fromJson(reader, Map.class);
+//            System.out.println(gson.toJson(map));
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
 }
